9fc5cdf859b005c3cc71f1b31ce5bc10a74c6bcd
Ziyobek <khazratkulov.zt@phystech.edu>
2021-05-19 15:10:37 +0500

    corrected dictionary and imports

--- serie_a.py
+++ serie_a.py
@@ -1,6 +1,6 @@
+import requests
 from flask import render_template, Blueprint
 from bs4 import BeautifulSoup
-import requests
 
 serie_a = Blueprint('serie_a', __name__, template_folder='templates')
 
@@ -15,16 +15,17 @@ def results_table():
     for team in league_table.find_all('tbody'):
         rows = team.find_all('tr')
         for row in rows:
-            table_info = dict()
-            table_info['place'] = row.find('td', class_='standing-table__cell').text
-            table_info['teams'] = row.find('td', class_='standing-table__cell standing-table__cell--name').text.strip()
-            table_info['played'] = row.find_all('td', class_='standing-table__cell')[2].text
-            table_info['wins'] = row.find_all('td', class_='standing-table__cell')[3].text
-            table_info['draws'] = row.find_all('td', class_='standing-table__cell')[4].text
-            table_info['losses'] = row.find_all('td', class_='standing-table__cell')[5].text
-            table_info['goals_for'] = row.find_all('td', class_='standing-table__cell')[6].text
-            table_info['goals_against'] = row.find_all('td', class_='standing-table__cell')[7].text
-            table_info['goals_difference'] = row.find_all('td', class_='standing-table__cell')[8].text
-            table_info['points'] = row.find_all('td', class_='standing-table__cell')[9].text
+            table_info = {
+                'place': row.find('td', class_='standing-table__cell').text,
+                'teams': row.find('td', class_='standing-table__cell standing-table__cell--name').text.strip(),
+                'played': row.find_all('td', class_='standing-table__cell')[2].text,
+                'wins': row.find_all('td', class_='standing-table__cell')[3].text,
+                'draws': row.find_all('td', class_='standing-table__cell')[4].text,
+                'losses': row.find_all('td', class_='standing-table__cell')[5].text,
+                'goals_for': row.find_all('td', class_='standing-table__cell')[6].text,
+                'goals_against': row.find_all('td', class_='standing-table__cell')[7].text,
+                'goals_difference': row.find_all('td', class_='standing-table__cell')[8].text,
+                'points': row.find_all('td', class_='standing-table__cell')[9].text
+            }
             create_table.append(table_info)
     return render_template("serie_a.html", create_table=create_table)
